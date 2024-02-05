0a1986402dbbba63006ec4b2876bd9ca62475f4e
unknown <teteulima@outlook.com>
2024-02-04 22:17:23 -0300

    added new things in sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { SideBarContext } from '../context/SideBarContext'
+import { BsBag } from 'react-icons/bs';
 
 const Header = () => {
+
+     // Use o hook useContext para acessar o contexto
+     const contextValue = useContext(SideBarContext);
+
+     // Verifica se o contexto não é undefined
+     if (!contextValue) {
+       console.error("O contexto da barra lateral não foi fornecido corretamente.");
+       return null; // ou qualquer outra lógica de tratamento para quando o contexto é undefined
+     }
+   
+     // Extrai as propriedades do contexto
+     const { isOpen, setIsOpen, handleCloser } = contextValue;
+
   return (
-    <div>Header</div>
+    <div>
+      <div>Header</div>
+      <div onClick={() => setIsOpen(!isOpen)}>
+        <BsBag className='text-2xl cursor-pointer flex relative' />
+      </div>
+    </div>
   )
 }
 
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import { useContext } from 'react'
+
+import {IoMdArrowForward} from 'react-icons/io'
+// import {FiTrash2} from 'react-icons/fi'
+import { SideBarContext } from '../context/SideBarContext'
+
 
 const SideBar = () => {
+   // Use o hook useContext para acessar o contexto
+   const contextValue = useContext(SideBarContext);
+
+   // Verifica se o contexto não é undefined
+   if (!contextValue) {
+     console.error("O contexto da barra lateral não foi fornecido corretamente.");
+     return null; // ou qualquer outra lógica de tratamento para quando o contexto é undefined
+   }
+ 
+   // Extrai as propriedades do contexto
+   const { isOpen, setIsOpen, handleCloser } = contextValue;
+
   return (
-    <div>SideBar</div>
+    <div className={`${isOpen ? 'right-0' : '-right-full' } w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:w-[30vw]
+    transition-all duration-300 z-20 px-4 lg:px-[35px]`}>
+
+      <div className='flex items-center justify-between py-6 border-b'>
+        <div>Shopping Bag (0)</div>
+        <div className='cursor-pointer w-8 h-8 flex justify-center items-center'>
+          <IoMdArrowForward onClick={() => setIsOpen(!isOpen)} className='text-2xl'/>
+        </div>
+      </div>
+    </div>
   )
 }
 
