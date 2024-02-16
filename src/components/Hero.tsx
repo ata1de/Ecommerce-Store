b6f5f9c3e43fbb34ba4d3cf2f5f88aebcfee6ec1
unknown <teteulima@outlook.com>
2024-02-16 14:32:50 -0300

    adjusted items-center of the title in hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <section className='bg-pink-200 h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24 '>
         <div className="container mx-auto flex justify-around h-full ">
-            <div className='flex flex-col justify-center'>
+            <div className='flex flex-col justify-center items-center'>
                 <div className='font-semibold flex items-center uppercase'>
                     <div className='w-10 h-[2px] bg-red-500 mr-3'></div>New Trend
                 </div>
