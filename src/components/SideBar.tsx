8c8761534338295d808cf79a5aee64a17b78f1c8
unknown <teteulima@outlook.com>
2024-02-27 15:18:23 -0300

    finishing cart

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -27,7 +27,7 @@ const SideBar = () => {
 
   console.log(cartNumber)
   return (
-    <div className={`${isOpen ? 'right-0' : '-right-full' } w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw]
+    <div className={`${isOpen ? 'right-0' : '-right-full' } w-full bg-white fixed top-0 h-screen shadow-2xl md:w-[35vw] xl:max-w-[30vw]
     transition-all duration-300 z-20 px-4 lg:px-[35px]`}>
       <div className='flex items-center justify-between py-6 border-b'>
         <div className='uppercase text-sm font-semibold'>Shopping Bag ({cartNumber})</div>
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -12,30 +12,35 @@ const Cart = () => {
       const { cart, total } = contextCart
   return (
     <div className="flex flex-col justify-center h-screen mx-5">
-        <h1 className="font-medium text-4xl">Carrinho</h1>
-        <div className="mt-4">
-            <table className='w-full'>
-                <thead>
-                    <tr>
-                        <th>Produto</th>
-                        <th>Preço</th>
-                        <th>Qtd.</th>
-                        <th>Total</th>
-                    </tr>
-                </thead>
-                <tbody className="">
-                    {cart?.map((item, index) => (
-                        <tr className="" key={index}>
-                            <td><img className="max-w-[90px]" src={item.image} alt={item.description} /></td>
-                            <td>R$ {item.price}</td>
-                            <td>{item.amount}</td>
-                            <td>R$ {(item.price) * (item.amount)}</td>
+        <div>
+            <h1 className="font-semibold text-6xl mb-5">Carrinho</h1>
+            <div className="mt-4 overflow-y-auto overflow-x-hidden h-[500px] lg:h-[600px]">
+                <table className='w-full'>
+                    <thead className="border-b-2">
+                        <tr>
+                            <th className="text-start font-extrabold text-xl">Produto</th>
+                            <th className="font-extrabold text-xl ">Preço</th>
+                            <th className="font-extrabold text-xl">Qtd.</th>
+                            <th className="font-extrabold text-xl">Total</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+
+                    <tbody className="">
+                        {cart?.map((item, index) => (
+                        <tr className="border-b-4 " key={index}>
+                            <td className=""><img className="max-w-[140px] ml-5 m-4" src={item.image} alt={item.description} /></td>
+                            <td className="text-center text-xl">R$ {item.price}</td>
+                            <td className="text-center text-xl">{item.amount}</td>
+                            <td className="text-center text-xl">R$ {(item.price) * (item.amount)}</td>
+                        </tr>
+                        ))}
+
+                    </tbody>
+                </table>
+            </div>
+            <div className="ml-auto mt-4 text-center font-semibold text-xl p-4 border-2 w-[260px]"><span>Total:</span> R$ {total.toFixed(2)}</div>
+
         </div>
-        <div className="mt-4"><span>Total:</span> R$ {total.toFixed(2)}</div>
     </div>
   )
 }
