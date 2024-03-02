9081851c3902b530ce7fd4ac03710f776a66793d
unknown <teteulima@outlook.com>
2024-03-02 14:52:56 -0300

    added framer-motion in project

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -3,6 +3,7 @@ import { TypeProduct } from "../types/types";
 import { BsPlus, BsEyeFill } from 'react-icons/bs';
 import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
+import { motion } from "framer-motion";
 
 const Product = ({product}: TypeProduct) => {
     const contextValue = useContext(CartContext);
@@ -22,11 +23,11 @@ const Product = ({product}: TypeProduct) => {
                         <img className="max-h-[160px] group-hover:scale-110 transition duration-300" src={product.image} alt={product.title} />
                     </div>
                     <div className="absolute top-6 -right-11 group-hover:right-5 p-2 flex flex-col justify-center items-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300">
-                        <button onClick={() => addToCart({product})}>
+                        <motion.button whileTap={{scale: 0.9}} onClick={() => addToCart({product})}>
                             <div className="flex justify-center items-center text-white w-12 h-12 bg-red-500">
                                 <BsPlus className="text-3xl" />
                             </div>
-                        </button>
+                        </motion.button>
                         <Link to={`/products/${product.id}`} className="w-12 h-12 bg-white flex justify-center items-center text-primary drop-shadow-xl">
                             <BsEyeFill />
                         </Link>
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -11,24 +11,24 @@ const Cart = () => {
     
       const { cart, total } = contextCart
   return (
-    <div className="flex flex-col justify-center h-screen mx-5">
-        <div className=" flex flex-col justify-center h-[380px] lg:h-[450px] md:h-[400px] xl:h-[600px] ">
-            <h1 className="font-semibold leading-[1.1] text-6xl mb-5">Carrinho</h1>
-            <div className="mt-4 overflow-y-auto overflow-x-hidden h-[350px]">
+    <div className="flex flex-col justify-center h-screen mx-[140px] max-[640px]:mx-4">
+        <div className=" flex flex-col justify-center h-[380px] lg:h-[450px] md:h-[400px] xl:h-[600px]">
+            <h1 className="font-semibold leading-[1.1] max-[800px]:text-4xl text-5xl mb-5">Carrinho</h1>
+            <div className="mt-4 overflow-y-auto overflow-x-hidden h-[350px] rounded-sm ">
                 <table className='w-full'>
                     <thead className="border-b-2">
                         <tr >
-                            <th className="text-start font-extrabold text-xl">Produto</th>
-                            <th className="font-extrabold text-xl ">Preço</th>
-                            <th className="font-extrabold text-xl">Qtd.</th>
-                            <th className="font-extrabold text-xl">Total</th>
+                            <th className="text-start font-extrabold text-xl max-[800px]:text-lg">Produto</th>
+                            <th className="font-extrabold text-xl max-[800px]:text-lg">Preço</th>
+                            <th className="font-extrabold text-xl max-[800px]:text-lg">Qtd.</th>
+                            <th className="font-extrabold text-xl max-[800px]:text-lg">Total</th>
                         </tr>
                     </thead>
 
-                    <tbody className="">
+                    <tbody >
                         {cart?.map((item, index) => (
                         <tr className="border-b-4 " key={index}>
-                            <td className=""><img className="max-w-[100px] sm:w-[50px] ml-5 m-4" src={item.image} alt={item.description} /></td>
+                            <td className=""><img className="max-w-[80px] max-[640px]:w-[50px] ml-5 m-4" src={item.image} alt={item.description} /></td>
                             <td className="text-center text-xl">R$ {item.price}</td>
                             <td className="text-center text-xl">{item.amount}</td>
                             <td className="text-center text-xl">R$ {(item.price) * (item.amount)}</td>
